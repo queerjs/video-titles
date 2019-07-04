0399e0b1defaea56d60661ebb122b684f4c57952
Lau Skeeter <lawr@lauragonzalez.cc>
2019-07-04 22:06:37 +0100

    ??

--- screenys.js
+++ screenys.js
@@ -6,14 +6,14 @@ const speakorz = [
 		name: 'Laura Gonzalez',
 		talk: ' typescript made enjoyable',
 		photo:
-			'https://pbs.twimg.com/profile_images/1092069028154216449/M-yDW4aY_400x400.jpg',
+			'https://pbs.twimg.com/profile_images/1123259650126774272/KOE88EYU_reasonably_small.jpg',
 	},
 	{
 		filename: 'sara',
 		name: 'Sara Vieira',
 		talk: 'No. typescript will NEVER be enjoyable',
 		photo:
-			'https://pbs.twimg.com/profile_images/1123259650126774272/KOE88EYU_reasonably_small.jpg',
+			'https://pbs.twimg.com/profile_images/1092069028154216449/M-yDW4aY_400x400.jpg',
 	},
 ];
 

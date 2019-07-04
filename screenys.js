803d61fc7505380f6394e4170e4a0024441e022b
Lau Skeeter <lawr@lauragonzalez.cc>
2019-07-04 22:02:24 +0100

    screenys

---
+++ screenys.js
@@ -0,0 +1,39 @@
+const puppeteer = require('puppeteer');
+
+const speakorz = [
+	{
+		filename: 'laura',
+		name: 'Laura Gonzalez',
+		talk: ' typescript made enjoyable',
+		photo:
+			'https://pbs.twimg.com/profile_images/1123259650126774272/KOE88EYU_reasonably_small.jpg',
+	},
+	{
+		filename: 'sara',
+		name: 'Sara Vieira',
+		talk: 'No. typescript will NEVER be enjoyable',
+		photo:
+			'https://pbs.twimg.com/profile_images/1123259650126774272/KOE88EYU_reasonably_small.jpg',
+	},
+];
+
+const shot = async ({ filename, ...data }) => {
+	const browser = await puppeteer.launch();
+	const page = await browser.newPage();
+	await page.goto('http://localhost:3000?data=' + JSON.stringify(data));
+	await page.setViewport({
+		width: 1920,
+		height: 1080,
+	});
+	await page.waitFor(1000);
+
+	await page.screenshot({ path: 'out/' + filename + '.png' });
+	console.log(filename);
+	await browser.close();
+};
+
+(async () => {
+	for (const key of speakorz) {
+		await shot(key);
+	}
+})();
--- src/App.js
+++ src/App.js
@@ -1,17 +1,29 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
-
-import { css } from '@emotion/core';
-
-const VideoScreen = () => {
-	return <div className={'canvas'}>hi</div>;
+import fly from './fly.svg';
+const VideoScreen = ({ photo, name, talk }) => {
+	return (
+		<div className={'canvas'}>
+			<div className="bow" />
+			<main>
+				<img alt="" src={photo} />
+				<div>
+					<h2>{name}</h2>
+					<h3>{talk}</h3>
+				</div>
+			</main>
+			<img src={fly} alt="queerJS" className="fly" />
+		</div>
+	);
 };
 
 function App() {
+	const params = new URLSearchParams(window.location.search);
+
 	return (
 		<div className="App">
-			<VideoScreen />
+			<VideoScreen {...JSON.parse(params.get('data'))} />
 			<header className="App-header">
 				<img src={logo} className="App-logo" alt="logo" />
 				<p>

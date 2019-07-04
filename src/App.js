3d87aa46ee399aa368d2c5e925f8057e13a88eb8
Lau Skeeter <lawr@lauragonzalez.cc>
2019-07-04 22:15:20 +0100

    remove the CRA stuff

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 import fly from './fly.svg';
+
 const VideoScreen = ({ photo, name, talk }) => {
 	return (
 		<div className={'canvas'}>
@@ -22,22 +22,8 @@ function App() {
 	const params = new URLSearchParams(window.location.search);
 
 	return (
-		<div className="App">
+		<div>
 			<VideoScreen {...JSON.parse(params.get('data'))} />
-			<header className="App-header">
-				<img src={logo} className="App-logo" alt="logo" />
-				<p>
-					Edit <code>src/App.js</code> and save to reload.
-				</p>
-				<a
-					className="App-link"
-					href="https://reactjs.org"
-					target="_blank"
-					rel="noopener noreferrer"
-				>
-					Learn React
-				</a>
-			</header>
 		</div>
 	);
 }

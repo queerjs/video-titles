51486713d46038113e0b84335e0ad5a875804dbc
Lau Skeeter <lawr@lauragonzalez.cc>
2019-07-04 22:05:29 +0100

    add my photo

--- screenys.js
+++ screenys.js
@@ -6,7 +6,7 @@ const speakorz = [
 		name: 'Laura Gonzalez',
 		talk: ' typescript made enjoyable',
 		photo:
-			'https://pbs.twimg.com/profile_images/1123259650126774272/KOE88EYU_reasonably_small.jpg',
+			'https://pbs.twimg.com/profile_images/1092069028154216449/M-yDW4aY_400x400.jpg',
 	},
 	{
 		filename: 'sara',
